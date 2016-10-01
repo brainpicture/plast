1772146fe789db6843b7a3cd26f42037f5fda7ce
brainfucker <oleg@emby.ru>
2016-10-02 01:31:29 +0300

    track changes

--- main.c
+++ main.c
@@ -1,40 +1,26 @@
 #include "lib/env.c"
 
-typedef struct ctx1 {
-  sds test;
-} ctx1;
-
 typedef struct ctx0 {
-  ctx1 this;
 } ctx0;
 
-void func_variable_plast_undefined(ctx1 *this);
-typedef struct ctx2 {
-} ctx2;
-
-sds func_plast_retTest_undefined();
-typedef struct ctx3 {
-  ctx1 plast;
-} ctx3;
+void func_string_test_integer(int *arg);
+typedef struct ctx1 {
+} ctx1;
 
 void func_undefined_main_undefined();
 
-void func_variable_plast_undefined(ctx1 *this) {
+void func_string_test_integer(int *arg) {
 struct ctx0 ctx;
-(*this).test = sdsnew("wow");
-
-}
-
-sds func_plast_retTest_undefined() {
-struct ctx2 ctx;
-return sdsnew("hey");
+printf("%s\n", (*this));
+printf("%d\n", (*arg));
 
 }
 
 void func_undefined_main_undefined() {
-struct ctx3 ctx;
-func_variable_plast_undefined(&ctx.plast);
-printf("%s\n", func_plast_retTest_undefined(&ctx.plast));
+struct ctx1 ctx;
+sds def0 = sdsnew("wow sdf");
+int def1 = 2;
+func_string_test_integer(&def0, &def1);
 
 }
 
